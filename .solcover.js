1fafe8e385784015ac608511385752e5c8674350
Aaron Boyd <aaronmboyd@gmail.com>
2022-02-13 15:42:29 +0100

    [CHORE] - Exclude mocks from linting

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,3 @@
 module.exports = {
-    skipFiles: ["mocks/NonCompliantContract.sol"]
-  };
+    skipFiles: ["mocks/ERC721Mock.sol", "mocks/ERC1155Mock.sol", "mocks/LinkMock.sol", "mocks/VRFCoordinatorMock.sol", "mocks/NonCompliantContract.sol", "lib/ABDKMath64x64.sol"]
+};
