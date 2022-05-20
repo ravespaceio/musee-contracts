4457a0aa16f5e7b8e4531750fffb5bb41575bbbc
Aaron Boyd <aaronmboyd@gmail.com>
2022-05-20 16:14:42 +0200

    [TEST] - Skip IPFS test

--- test/ipfs.test.ts
+++ test/ipfs.test.ts
@@ -10,7 +10,7 @@ describe("IPFS upload and pinning...", () => {
     const pinataKey : string  = process.env.PINATA_KEY || "undefined";
     const pinataSecret: string = process.env.PINATA_SECRET || "undefined";
 	
-    it("Should upload a folder of images to IPFS and pin it with Pinata", async function () {
+    xit("Should upload a folder of images to IPFS and pin it with Pinata", async function () {
         images = await pinDirectoryToIPFS(pinataKey, pinataSecret, "./test/utils/test_images",  "/*.jpeg");
 		expect(images).to.be.not.empty;
         expect(images).to.have.property("cid");
@@ -18,14 +18,14 @@ describe("IPFS upload and pinning...", () => {
         expect(images).to.have.property("file");
 	});
 
-    it("Should post the same images to Arweave", async function () {        
+    xit("Should post the same images to Arweave", async function () {        
         for(let i = 0;i<images.files.length;i++){
             const result = await postToArweave(images.files[i].cid);
             expect(result).to.be.not.empty;
         }
 	});
 
-    it("Should upload a folder of JSON files to IPFS and pin it with Pinata", async function () {
+    xit("Should upload a folder of JSON files to IPFS and pin it with Pinata", async function () {
         metadata = await pinDirectoryToIPFS(pinataKey, pinataSecret, "./test/utils/test_metadata",  "/*");
         expect(metadata).to.be.not.empty;
         expect(metadata).to.have.property("cid");
